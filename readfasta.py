d2866ccfca074e02dc1594fd1e66defbc6e1c5c4
nabarunr3 <nabarunr3@gmail.com>
2021-02-13 11:30:28 +0530

    Some progress in fasta file reading, but not yet able to debug the code to store it in a dictionary

--- readfasta.py
+++ readfasta.py
@@ -1,5 +1,5 @@
 
-#!/usr/bin/python3.9
+#!/usr/bin/python3.8
 
 """
 four tasks at hand:
@@ -18,6 +18,47 @@ four tasks at hand:
 
 import sys
 
-fasta_file = sys.argv[]
+#now we will take the second argument of our command line input
+fasta_file = sys.argv[1]
 
+#try to open, read the file and store in a string
+try:
+	open(fasta_file)
+except IOError:		#pass exception of cannot open
+	print("File nonexistant")
+
+#now we define a function for storing the file in the dictionary
+
+def fastadict(fastafile):
+	"""
+	This function reads a fasta file and stores in a dictionary
+
+	"""
+	fasta_dict = {}	#initializing empty dictionary
+	for line in fastafile:
+		line = line.rstrip()	#removing newline characters
+		if line[0] == '>':
+			key = line
+			"""Assigning the line as key. Depending on the header,
+			individual words might also be assigned as headers
+			by splitting the header using
+
+			line.split(splitting character, default whitespace)
+
+			This stores the word strings in a list."""
+
+			fasta_dict[key] = ''
+		else:	#sequence, not header
+			fasta_dict[key] = fasta_dict[key] + line
+			"""The 'key' key is used because key variable is
+			already initialized with the previous header.
+			A new header will automatically be initialized with
+			as a new key, because of the if codeblock."""
+
+	close(fastafile)
+	return fasta_dict
+
+dict = fastadict(fasta_file)
+for header, sequence in dict.items():
+	print(header, sequence)
 
