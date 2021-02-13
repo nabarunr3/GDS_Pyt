8f4e18bcf68efa990be8d6e7880ae2943dcc4ef7
nabarunr3 <nabarunr3@gmail.com>
2021-02-13 08:22:21 +0530

    Added instructions and started constructing readfasta module

---
+++ readfasta.py
@@ -0,0 +1,23 @@
+
+#!/usr/bin/python3.9
+
+"""
+four tasks at hand:
+
+1. read the fasta file in a dictionary and count the number of sequence records
+2. Lengths of the sequences in a file returned as a dictionary. 
+3. Identify all the ORFs of each sequence present in the FASTA file. 
+	Also identify the identifier containing the longest ORF.
+	For a given Identifier what is the longest ORF contained in that sequence?
+	The starting position of the longest ORF?
+4. Given a length n, identify all repeats of length n in all the sequneces in the file. 
+	Also how many times each repeat occurs in the file. 
+	Which is the most frequent repeat of a given length. 
+
+"""
+
+import sys
+
+fasta_file = sys.argv[]
+
+
