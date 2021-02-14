6fb47f112ce4bf3ccd3b1cefa4732b20d004479c
nabarunr3 <nabarunr3@gmail.com>
2021-02-14 08:56:01 +0530

    Working on getting reading frames

--- readingframe.py
+++ readingframe.py
@@ -15,7 +15,7 @@ try:
 except IOError:
 	print("File nonexistant")
 
-def fileoriffind(fasta_string):
+def file_orf_find(fasta_string):
 	"""
 	This function displays all the orfs from a given fasta file
 	and finds the longest orf
@@ -25,14 +25,34 @@ def fileoriffind(fasta_string):
 	"""fasta_dict contains the identifier-sequence pairs from all 
 	the sequences in the fasta file, as key-value pairs in a dictionary"""
 
-def orifind(seq):
-	rf1 = fasta_dict[identf][0:]
-	rf2 = fasta_dict[identf][1:]
-	rf3 = fasta_dict[identf][2:]
+def orf_find(seq):
+	framedict = {}
 
-	rf = 0
-		for rf in range(len(rf) - 1):
-			codon = 
+	#extract strings of all 3 frames of reference
+	frame_no = 1
+	for frame_no in range(3):
+		framedict["rf" + str(frame_no)] = seq[i:]
 
+	#eliminate trailing letters which arent multiples of 3
+	for frame in framedict:
+		seq_trimmed_end = len(framedict[frame]) - (len(framedict[frame])%3)
+		framedict[frame] = framedict[frame][:seq_trimmed_end]
+
+	#calculating the lengths of the ORFs
+	stop_codon_tuple = ("TAA", "TAG", "TGA")
+	orf_len = {}
+	frame_no = 1
+	for frame in framedict:
+		for index in len(framedict[frame]):
+			codon = framedict[frame][index:index + 3]
+			if codon == "ATG":
+				orf_start = index + frame_no
+			else if codon in stop_codon_tuple:
+				orf_stop = index + frame_no
+			
+			orf_len[frame]  = orf_stop - orf_start
+			
+			index = index + 3
+		frame_no = frame_no + 1
 
 
