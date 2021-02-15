08dd3ea34d3732ae4e7da3a53a1a6a378cee94e4
nabarunr3 <nabarunr3@gmail.com>
2021-02-16 01:14:47 +0530

    Towards reading frame

--- readingframe.py
+++ readingframe.py
@@ -15,6 +15,45 @@ try:
 except IOError:
 	print("File nonexistant")
 
+
+def orf_find(seq):
+	framedict = {}
+	start_codon = "ATG"
+	stop_codon_tuple = ("TAA", "TAG", "TGA")
+
+	for frame in range(2):	#frame will run 0,1,2
+		seq_frame = seq[frame:]	#setting the start position
+		start_positions = []
+		framedict["frame" + str(frame + 1)] = [] 
+		"""initializing the ORF list corresponding to the 
+		frame of reference"""
+
+		for index in len(seq[frame]):
+			codon = seq_frame[index:(index + 3)]
+			#going codon by codon
+
+			if codon == start_codon:
+				start_posistions.append(index + frame)
+
+			if codon in stop_codon_tuple and start_pos != []:
+				#we would want a start codon 
+				#preceeding a stop codon
+				stop_pos = index + frame
+
+				for start_codon_positions in start_pos:
+					framedict["frame" + str(frame + 1)].append(\
+					[start_codon_pos, stop_pos, (stop_pos - start_codon_pos)])
+					"""this should consider as ORFs the sequences
+					between all start codons before the discovered stop codon"""
+
+				start_pos = [] #empyting the start codon position buffer
+
+			index = index + 3 #so that the window reads three bases at a time w/o overlap
+	return framedict
+
+
+
+
 def fasta_ORFs(fasta_string):
 	"""
 	This function displays the positions of all the orfs from 
@@ -26,12 +65,14 @@ def fasta_ORFs(fasta_string):
 	"""fasta_dict contains the identifier-sequence pairs from all 
 	the sequences in the fasta file, as key-value pairs in a dictionary"""
 
-	by_frame_ORF_list_dict = {}
-	"""this is a dictionary where each key-value pair corresponds to 
+	by_seq_by_frame_ORF_list_dict = {}
+	"""this is a dictionary where each key-value pair represents 
 	the identifier of a sequence in the fasta file and a corresponding 
 	*nested dictionary*. This *nested dictionary* contains 3key-value 
 	pairs, where the keys are three reading frames, and each reading 
-	frame corresponds to a *list* containing all the valid ORF lengths."""  
+	frame corresponds to a *list* containing more *nested lists*,
+	each containing the following:
+	[start_position, stop_position, ORF length]"""  
 
 	for identifier in fasta_dict:
 		by_frame_ORF_list_dict[identifier] = {}
@@ -41,53 +82,12 @@ def fasta_ORFs(fasta_string):
 		key-value pairs, where each key represents a reading frame..
 		Each reading frame stores a list of all the ORFs."""
 
-`	return by_seq_by_frame_ORF_list_dict
+	return by_seq_by_frame_ORF_list_dict
 
-def orf_find(seq):
-	framedict = {}
-	ORF_list = []
-	stop_codon_tuple = ("TAA", "TAG", "TGA")
-	for frame in range(2):
-		seq_frame = seq[frame:]
-		start_pos = [None]
-		start_codon_count = 0
-		framedict["frame" + str(frame + 1)] = []
-		for index in len(seq[frame]):
-			codon = seq_frame[index:(index + 3)]
-			if codon == "ATG":
-				start_codon_count = start_codon_count + 1
-				start_pos[start_codon_count]  = index + frame
-			if codon in stop_tuple and start_pos != [None]:
-				framelist["frame" + str(frame + 1) = 
-				
-
-
+def orf_compare(input_frame):
 	"""
-	#extract strings of all 3 frames of reference
-	frame_no = 1
-	for frame_no in range(3):
-		framedict["rf" + str(frame_no)] = seq[i:]
+	This function takes in a complex nested ORF containing dictionary.
+	It then compares the length of the ORF 
+	"""
 
-	#eliminate trailing letters which arent multiples of 3
-	for frame in framedict:
-		seq_trimmed_end = len(framedict[frame]) - (len(framedict[frame])%3)
-		framedict[frame] = framedict[frame][:seq_trimmed_end]
-
-	#calculating the lengths of the ORFs
-	stop_codon_tuple = ("TAA", "TAG", "TGA")
-	orf_len = {}
-	frame_no = 1
-	for frame in framedict:
-		for index in len(framedict[frame]):
-			codon = framedict[frame][index:index + 3]
-			if codon == "ATG":
-				orf_start = index + frame_no
-			else if codon in stop_codon_tuple:
-				orf_stop = index + frame_no
-			
-			orf_len[frame]  = orf_stop - orf_start
-			
-			index = index + 3
-		frame_no = frame_no + 1
-		"""
-
+for i
